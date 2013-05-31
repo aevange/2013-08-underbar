cbed255bf96b6883deea633b05c0828f6cd5b2ae
Andre Evangelesta <andre.evangelesta@gmail.com>
2013-05-31 08:06:31 -0700

    Add body to _.map()

--- src/underbar.js
+++ src/underbar.js
@@ -113,6 +113,12 @@ var _ = { };
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var rArray = [];
+    var pushIter = function (value) {
+      rArray.push(iterator(value));
+    };
+    _.each(array, pushIter);
+    return rArray;
   };
 
   /*
