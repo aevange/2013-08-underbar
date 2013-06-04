dbef3dfa93cf0834e93a59d5c888c1df9a99356e
Andre Evangelesta <andre.evangelesta@gmail.com>
2013-06-03 20:13:54 -0700

    Add body to _.every()

--- src/underbar.js
+++ src/underbar.js
@@ -184,6 +184,11 @@ var _ = { };
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(noFalse, item) {
+      if(!noFalse) return false;
+      var test = (iterator!==undefined) ? iterator(item): item;
+      return test == true;
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
